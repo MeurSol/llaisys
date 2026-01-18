f3ef7f56f8da2bff894d48a2a6a71bfa6df501e9
MeurSol <2569539312@qq.com>
2026-01-18 12:53:37 +0800

    feat2.4 RMS_norm

---
+++ src/ops/rms_norm/cpu/rms_norm_cpu.cpp
@@ -0,0 +1,47 @@
+#include "rms_norm_cpu.hpp"
+#include "../../../utils.hpp"
+#include <type_traits>
+#include <cmath>
+template <typename T>
+void rms_norm_(T *out, const T *in, const T *weight,float eps, size_t N, size_t D) {
+    using acc_t = std::conditional_t<std::is_same_v<T, llaisys::fp16_t> || std::is_same_v<T, llaisys::bf16_t>, float, T>;
+
+    for (size_t i = 0; i < N; ++i) {
+        double square_sum = acc_t{};
+        for (size_t j = 0; j < D; ++j) {
+            if constexpr(std::is_same_v<T, acc_t>) {
+                square_sum += in[i*D+j]*in[i*D+j];
+            } else {
+                auto x = llaisys::utils::cast<acc_t>(in[i*D + j]);
+                square_sum += x*x;
+            }
+        }
+        auto RMS = std::sqrt(square_sum/D+eps);
+        for (size_t j = 0; j < D; ++j) {
+            if constexpr (std::is_same_v<T, acc_t>) {
+                out[i * D + j] = (in[i * D + j] / RMS) * weight[j];
+            } else {
+                auto x = llaisys::utils::cast<acc_t>(in[i * D + j]);
+                auto w = llaisys::utils::cast<acc_t>(weight[j]);
+                out[i * D + j] = llaisys::utils::cast<T>((x / RMS) * w);
+            }
+        }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void rms_norm(std::byte *out, const std::byte *in, const std::byte *weight,float eps, llaisysDataType_t dtype, size_t N, size_t D) {
+    switch (dtype) {
+    case LLAISYS_DTYPE_F32:
+        return rms_norm_(reinterpret_cast<float *>(out), reinterpret_cast<const float *>(in), reinterpret_cast<const float *>(weight),eps, N, D);
+    case LLAISYS_DTYPE_BF16:
+        return rms_norm_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const llaisys::bf16_t *>(in),
+                    reinterpret_cast<const llaisys::bf16_t *>(weight),eps, N, D);
+    case LLAISYS_DTYPE_F16:
+        return rms_norm_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const llaisys::fp16_t *>(in),
+                    reinterpret_cast<const llaisys::fp16_t *>(weight),eps, N, D);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(dtype);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/rms_norm/op.cpp
+++ src/ops/rms_norm/op.cpp
@@ -1,7 +1,34 @@
 #include "op.hpp"
 
+#include "../../utils.hpp"
+
+#include "cpu/rms_norm_cpu.hpp"
+
 namespace llaisys::ops {
 void rms_norm(tensor_t out, tensor_t in, tensor_t weight, float eps) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in, weight);
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype(), weight->dtype());
+    ASSERT(in->isContiguous() && weight->isContiguous(), "RMS Norm: inputs must be contiguous.");
+
+    size_t N = in->shape()[0]; 
+    size_t D = in->shape()[1]; 
+    // always support cpu calculation
+    if (in->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::rms_norm(out->data(), in->data(), weight->data(), eps, in->dtype(), N, D);
+    }
+
+    llaisys::core::context().setDevice(in->deviceType(), in->deviceId());
+
+    switch (in->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::rms_norm(out->data(), in->data(), weight->data(), eps, in->dtype(), N, D);
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
